5f9f042038082226d215c2f5b64eef7b02153dd8
18865501555 <66073067+18865501555@users.noreply.github.com>
2020-06-24 23:39:36 +0800

    update

--- src/main/java/com/webserver/core/ClientHandler.java
+++ src/main/java/com/webserver/core/ClientHandler.java
@@ -22,15 +22,32 @@ public class ClientHandler implements Runnable{
     public void run() {
         try {
             System.out.println("ClientHandler:开始处理...");
+            //测试读取客户端发送过来的请求的内容
             InputStream in = socket.getInputStream();
             int d = 0;
+            StringBuilder builder = new StringBuilder();
             while ((d=in.read())!=-1){
-                System.out.println((char)d);
+                char c = (char) d;
+                //上一次读取若是回车符并且本次读取的是换行符就停止
+                if (builder.length()!=0 && builder.charAt(builder.length()-1)==13 && c==10){
+                    break;
+                }
+                builder.append(c);
             }
+            String line = builder.toString().trim();
+            System.out.println(line);
+
+            //1解析请求
+
+            //2处理请求
+
+            //3响应客户端
+
             System.out.println("ClientHandler:处理完毕!");
         } catch (IOException e) {
             e.printStackTrace();
         } finally {
+            //断开连接（HTTP1.0协议要求，一问一答后断开连接）
             try {
                 socket.close();
             } catch (IOException e) {

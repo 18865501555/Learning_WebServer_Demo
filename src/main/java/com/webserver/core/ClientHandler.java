3da20e646168575b0d9914dc572ac959ec2ee3b4
18865501555 <66073067+18865501555@users.noreply.github.com>
2020-06-25 23:30:15 +0800

    update_v6

--- src/main/java/com/webserver/core/ClientHandler.java
+++ src/main/java/com/webserver/core/ClientHandler.java
@@ -24,52 +24,84 @@ public class ClientHandler implements Runnable{
     public void run() {
         try {
             System.out.println("ClientHandler:开始处理...");
-            //测试读取客户端发送过来的请求的内容
+            //1解析请求
             System.out.println("ClientHandler:解析请求");
             HttpRequest request = new HttpRequest(socket);
 
-            //1解析请求
-
             //2处理请求
-
-            //3响应客户端
             /*
-            发送一个HTTP响应给客户端
-            内容是：webapp/myweb/index.html
+            通过请求对象获取抽象路径
              */
-            OutputStream out = socket.getOutputStream();
-            File file = new File("./src/main/webapp/myweb/index.html");
-            //3.1发送状态行
-            //HTTP/1.1 200 OK
-            String line = "HTTP/1.1 200 OK";
-            byte[] data = line.getBytes("ISO8859-1");
-            out.write(data);
-            out.write(13);//单独发送回车符
-            out.write(10);//单独发送换行符
+            String path = request.getUri();
+            System.out.println("抽象路径:"+path);
+            File file = new File("./src/main/webapp"+path);
+            //判断用户请求的资源是否真实存在
+            if (file.exists()){
+                System.out.println("资源已找到!");
+                //存在则将该资源响应给客户端
+                OutputStream out = socket.getOutputStream();
+                //3.1发送状态行
+                //HTTP/1.1 200 OK
+                String line = "HTTP/1.1 200 OK";
+                byte[] data = line.getBytes("ISO8859-1");
+                out.write(data);
+                out.write(13);//单独发送回车符
+                out.write(10);//单独发送换行符
 
-            //3.2发送响应头
-            //Content-Type: text/html
-            line = "Content-Type: text/html";
-            data = line.getBytes("ISO8859-1");
-            out.write(data);
-            out.write(13);
-            out.write(10);
-            //Content-Length: xxxx
-            line = "Content_Length:" + file.length();
-            data = line.getBytes("ISO8859-1");
-            out.write(data);
-            out.write(13);
-            out.write(10);
-            //单独发送回车符换行符表示响应头部分发送完毕
-            out.write(13);
-            out.write(10);
+                //3.2发送响应头
+                //Content-Type: text/html
+                line = "Content-Type: text/html";
+                data = line.getBytes("ISO8859-1");
+                out.write(data);
+                out.write(13);
+                out.write(10);
+                //Content-Length: xxxx
+                line = "Content_Length:" + file.length();
+                data = line.getBytes("ISO8859-1");
+                out.write(data);
+                out.write(13);
+                out.write(10);
+                //单独发送回车符换行符表示响应头部分发送完毕
+                out.write(13);
+                out.write(10);
 
-            //3.3发送消息正文
-            FileInputStream fis = new FileInputStream(file);
-            int len = 0;
-            byte[] buf = new byte[1024 * 10];
-            while ((len = fis.read(buf)) != -1) {
-                out.write(buf, 0, len);
+                //3.3发送响应正文
+                FileInputStream fis = new FileInputStream(file);
+                int len = 0;
+                byte[] buf = new byte[1024 * 10];
+                while ((len = fis.read(buf)) != -1) {
+                    out.write(buf, 0, len);
+                }
+            } else {
+                //不存在则响应404
+                System.out.println("资源不存在!");
+                File notFound = new File("./src/main/webapp/root/404.html");
+                OutputStream out = socket.getOutputStream();
+                //发送状态行
+                String line = "HTTP/1.1 404 NotFound";
+                byte[] data = line.getBytes("ISO8859-1");
+                out.write(data);
+                out.write(13);
+                out.write(10);
+
+                line = "Content-Type: text/html";
+                data = line.getBytes("ISO8859-1");
+                out.write(data);
+                out.write(13);
+                out.write(10);
+                line = "Content-Length:"+notFound.length();
+                data = line.getBytes("ISO8859-1");
+                out.write(data);
+                out.write(13);
+                out.write(10);
+                out.write(13);
+                out.write(10);
+                FileInputStream fis = new FileInputStream(notFound);
+                int len = 0;
+                byte[] buf = new byte[1024*10];
+                while ((len = fis.read(buf))!=-1){
+                    out.write(buf,0,len);
+                }
             }
             System.out.println("响应发送完毕!");
 

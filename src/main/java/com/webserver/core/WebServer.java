3431a670f5370f4285383dbbe5c8e996fdc3ad92
18865501555 <66073067+18865501555@users.noreply.github.com>
2020-06-29 17:48:12 +0800

    update_v18

--- src/main/java/com/webserver/core/WebServer.java
+++ src/main/java/com/webserver/core/WebServer.java
@@ -3,6 +3,8 @@ package com.webserver.core;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 /**
  * WebServer是模拟Tomcat实现的一个Web容器
@@ -15,11 +17,12 @@ import java.net.Socket;
  */
 public class WebServer {
     private ServerSocket server;
-
+    private ExecutorService threadPool;
     public WebServer() {
         try {
             System.out.println("正在启动服务端...");
             server = new ServerSocket(8088);
+            threadPool = Executors.newFixedThreadPool(50);
             System.out.println("服务端启动完毕!");
         } catch (IOException e) {
             e.printStackTrace();
@@ -32,8 +35,7 @@ public class WebServer {
                 Socket socket = server.accept();
                 System.out.println("一个客户端连接了!");
                 ClientHandler handler = new ClientHandler(socket);
-                Thread t = new Thread(handler);
-                t.start();
+                threadPool.execute(handler);
             }
         } catch (IOException e) {
             e.printStackTrace();
